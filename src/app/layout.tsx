e4bdb691cb9e83363dc9554cea055c2c95415f37
sujeong <47809977+sjkim1014@users.noreply.github.com>
2026-08-11 09:19:45 +0900

    feat: GTM(Google Tag Manager) 컨테이너 스니펫 추가 (GTM-NZSF98NV)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -68,6 +68,21 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body>
+        <Script id="gtm" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-NZSF98NV');`}
+        </Script>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NZSF98NV"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <Script src="/apps/daelimEUEM_begin.js" strategy="beforeInteractive" />
         <Script src="/js/daelimEUEM_run.js" strategy="beforeInteractive" />
         <LenisScrollProvider>
