e1d6cea249af1e57465e0a706f7d7f57ece2e5fc
comgsu <abbgnog@gmail.com>
2026-08-19 10:29:39 +0900

    header에 corp,coop,csp,rp 주석. IIS에서 설정

--- next.config.ts
+++ next.config.ts
@@ -207,26 +207,26 @@ const nextConfig: NextConfig = {
             key: "Strict-Transport-Security",
             value: "max-age=31536000; includeSubDomains",
           },
-          {
-            key: "Referrer-Policy",
-            value: "strict-origin-when-cross-origin",
-          },
-          {
-            key: "Cross-Origin-Resource-Policy",
-            value: "same-origin",
-          },
-          {
-            key: "Cross-Origin-Opener-Policy",
-            value: "same-origin",
-          },
+          // {
+          //   key: "Referrer-Policy",
+          //   value: "strict-origin-when-cross-origin",
+          // },
+          // {
+          //   key: "Cross-Origin-Resource-Policy",
+          //   value: "same-origin",
+          // },
+          // {
+          //   key: "Cross-Origin-Opener-Policy",
+          //   value: "same-origin",
+          // },
           {
             key: "X-Frame-Options",
             value: "SAMEORIGIN",
           },
-          {
-            key: "Content-Security-Policy",
-            value: "frame-ancestors 'self'",
-          },
+          // {
+          //   key: "Content-Security-Policy",
+          //   value: "frame-ancestors 'self'",
+          // },
         ],
       },
     ];
