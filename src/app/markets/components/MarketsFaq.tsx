8da364008f91d4fee67bbc9c0db59d90a3e4337d
윤상기 <timesky82@gmail.com>
2026-09-07 14:21:18 +0900

    [PUB] 텍스트 수정

--- src/app/markets/components/MarketsFaq.tsx
+++ src/app/markets/components/MarketsFaq.tsx
@@ -10,7 +10,8 @@ export default function MarketsFaq({ items = faqItems }: MarketsFaqProps) {
     <CommonFaq
       description={
         <>
-          Learn how LS ELECTRIC&apos;s tailored power and automation solutions are engineered to solve your specific market challenges and accelerate your critical business objectives
+          Learn how LS ELECTRIC's tailored power and automation solutions are engineered to solve your specific market challenges and accelerate your critical business objectives. <br />
+          to solve your specific market challenges and accelerate your critical business objectives.
         </>
       }
       items={items}
