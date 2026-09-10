8cd6d18fac463536aa332a881f40ea5a8173a541
윤상기 <timesky82@gmail.com>
2026-09-10 09:37:58 +0900

    [pub]gnb

--- src/components/layout/shared/GnbMegaPanel.tsx
+++ src/components/layout/shared/GnbMegaPanel.tsx
@@ -16,7 +16,7 @@ type GnbMegaPanelProps = {
   onClose?: () => void;
 };
 
-const DEPTH_HOVER_DELAY_MS = 50;
+const DEPTH_HOVER_DELAY_MS = 200;
 
 function findDepth3(
   categories: GnbMegaDepth2[],
